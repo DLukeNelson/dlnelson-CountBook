c6adffb7f9b83c5343ac278d41f642fa087a6eb0
D. Luke Nelson <dlnelson@ualberta.ca>
2017-09-25 11:48:31 -0600

    Create a counter class.

---
+++ app/src/main/java/dlnelson/dlnelson_countbook/Counter.java
@@ -0,0 +1,83 @@
+package dlnelson.dlnelson_countbook;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Luke on 2017-09-25.
+ */
+
+public class Counter {
+    private String name;
+    private Date date;
+    private int currentValue;
+    private int initialValue;
+    private String comment;
+
+    public Counter(String name, int value, String comment) {
+        this.name = name;
+        this.updateDate();
+        this.currentValue = value;
+        this.initialValue = value;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+    private void updateDate() {
+        this.date = new Date();
+    }
+
+    public int getCurrentValue() {
+        return this.currentValue;
+    }
+    public void setCurrentValue(int value) {
+        this.currentValue = value;
+        this.updateDate();
+    }
+
+    public int getInitialValue() {
+        return this.initialValue;
+    }
+    public void setInitialValue(int value) {
+        this.initialValue = value;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void incrementValue() {
+        this.currentValue += 1;
+        this.updateDate();
+    }
+
+    public void decrementValue() {
+        if (this.currentValue > 0) {
+            this.currentValue -= 1;
+            this.updateDate();
+        }
+    }
+
+    public void resetValue() {
+        this.currentValue = this.initialValue;
+    }
+
+    public String toString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return this.name + "  " + dateFormat.format(this.date) + " : " + this.currentValue;
+    }
+
+
+}
