dfd4b0e5f99e90e3eacee1b35573f1f91c385953
D. Luke Nelson <dlnelson@ualberta.ca>
2017-09-26 17:56:53 -0600

    Create layout for edit counter activity.

---
+++ app/src/main/java/dlnelson/dlnelson_countbook/CounterActivity.java
@@ -0,0 +1,13 @@
+package dlnelson.dlnelson_countbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_counter);
+    }
+}
